11fa54cd30cb5b497f3b4b7c5448bc134cd05822
Minna Päivinen <MinnaAntti@Minna-Air.home>
2019-10-24 20:09:18 +0300

    Added memmove

--- libft/main.c
+++ libft/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 void *ft_memccpy(void * dst, const void * src, int c, size_t n);
+void *ft_memmove(void *dst, const void *src, size_t len);
 
 int main()
 {
@@ -17,5 +18,22 @@ int main()
 
     printf("%s\n", str3);
 
-;	return(0);
+    /***Test of memmove********/
+
+    char mstr[50] = "Shpongle - Tales of the inexpressible";
+    char e_mstr[50];
+
+    char ft_mstr[50] = "Shpongle - Tales of the inexpressible";
+    char ft_e_mstr[50];
+
+    memmove(&e_mstr, &mstr, 25);
+
+    printf("%s\n", e_mstr);
+    printf("%s\n", mstr);
+
+    ft_memmove(&ft_e_mstr, &ft_mstr, 25);
+
+    printf("%s\n", ft_e_mstr);
+    printf("%s\n", ft_mstr);
+	return(0);
 }
