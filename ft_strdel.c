795cc7916fa0ab4f3be77fb4e81f73e0453cc9ef
Antti Vornanen <anttivornanen@Antti-iMac.home>
2019-11-02 07:25:08 +0200

    fixed ft_strdel. Now using ft_memdel

--- ft_strdel.c
+++ ft_strdel.c
@@ -10,13 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
 
 void	ft_strdel(char **as)
 {
-	if (as)
-	{
-		free(as);
-	}
-	as = NULL;
+	ft_memdel((void**)as);
 }
