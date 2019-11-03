51bfadde67bf25f5f8c8c20f56ee4cfecb656942
Antti Vornanen <anttivornanen@Antti-iMac.home>
2019-11-03 16:55:11 +0200

    fixed edgecases on strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -10,14 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_strequ(char const *s1, char const *s2)
 {
-	while (*s1 && *s2)
+	int i;
+
+	i = 0;
+	if (s1[0] == 0 && s2[0] == 0)
+		return (1);
+	if (s1[0] == 0 || s2[0] == 0)
+		return (0);
+	while (s1[i] && s2[i])
 	{
-		if (*s1 != *s2)
+		if (s1[i + 1] != s2[i + 1] || s1[i] != s2[i])
 			return (0);
-		s1++;
-		s2++;
+		i++;
 	}
 	return (1);
 }
